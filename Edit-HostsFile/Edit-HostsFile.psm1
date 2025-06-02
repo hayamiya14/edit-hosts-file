2254d4cc4891822d76cfe46f050e811802713f38
hayamiya <0510ttk@gmail.com>
2025-06-02 17:44:12 +0900

    initial commit

---
+++ Edit-HostsFile/Edit-HostsFile.psm1
@@ -0,0 +1,15 @@
+﻿function Edit-HostsFile {
+    [Alias('ehf')]
+    [CmdletBinding()]
+    param()
+
+    $hostFilePath = 'C:\Windows\System32\drivers\etc\hosts'
+
+    if (-not (Test-Path -LiteralPath $hostFilePath)) {
+        Write-Error "hosts file not found at path: $EnvFile"
+        Return
+    }
+    Start-Process notepad $hostFilePath -Verb RunAs
+}
+
+Export-ModuleMember -Function 'Edit-HostsFile' -Alias 'ehf'

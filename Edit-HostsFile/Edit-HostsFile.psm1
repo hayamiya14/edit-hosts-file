00292241820decadd471a72b2b7c1ae1b9a958e7
hayamiya <0510ttk@gmail.com>
2025-06-03 14:00:41 +0900

    [fix] Write-Errorの引数を修正 その他、微修正

--- Edit-HostsFile/Edit-HostsFile.psm1
+++ Edit-HostsFile/Edit-HostsFile.psm1
@@ -1,15 +1,33 @@
-﻿function Edit-HostsFile {
-    [Alias('ehf')]
-    [CmdletBinding()]
-    param()
+﻿<#
+.SYNOPSIS
+    Opens the system Hosts file in Notepad with elevation.
 
+.DESCRIPTION
+    The Edit-HostsFile function locates the Windows Hosts file (typically at
+    C:\Windows\System32\drivers\etc\hosts) and opens it in Notepad running
+    as Administrator. This allows modifying the Hosts file without manually
+    navigating to the directory.
+
+.EXAMPLE
+    Import-Module Edit-HostsFile
+    Edit-HostsFile
+
+    Opens the Hosts file in Notepad (elevated). Alias 'ehf' can also be used:
+    ehf
+
+#>
+function Edit-HostsFile {
+    [CmdletBinding()]
+
+    # Define the standard path to the Hosts file on Windows.
     $hostFilePath = 'C:\Windows\System32\drivers\etc\hosts'
 
     if (-not (Test-Path -LiteralPath $hostFilePath)) {
-        Write-Error "hosts file not found at path: $EnvFile"
-        Return
+        Write-Error "Hosts file not found at path: $hostFilePath"
+        return
     }
-    Start-Process notepad $hostFilePath -Verb RunAs
+
+    Start-Process -FilePath 'notepad.exe' -ArgumentList $hostFilePath -Verb RunAs
 }
 
 Export-ModuleMember -Function 'Edit-HostsFile' -Alias 'ehf'
